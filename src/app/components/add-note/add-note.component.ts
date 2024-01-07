3c260e28e8fcc0c407d67d9f9e45b0ac3fbf1245
Arckanna <valerie.lecoeur75@gmail.com>
2024-01-07 14:24:11 +0100

    form

--- src/app/components/add-note/add-note.component.ts
+++ src/app/components/add-note/add-note.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-note',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-note.component.html',
   styleUrl: './add-note.component.scss'
 })
 export class AddNoteComponent {
-
+ addNoteForm= new FormGroup({
+  title: new FormControl(''),
+  text: new FormControl(''),
+ });
+ addNote() {
+  let title = this.addNoteForm.value.title ?? '';
+  let text = this.addNoteForm.value.text ?? '';
+  console.log(this.addNoteForm.value);
+ }
 }
+
