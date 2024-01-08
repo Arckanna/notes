9c01e26a81bc6924d932e87e62f0346196d1515d
Arckanna <valerie.lecoeur75@gmail.com>
2024-01-08 22:50:52 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
+import { AddNoteComponent } from './components/add-note/add-note.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   
+    {
+        path: '',
+        component: NotesListComponent,
+    },
+    {
+        path:'new',
+        component: AddNoteComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'notes',
+    },
+];
